8fa5e050495e92f08605b09510265b6a5d8e0783
Christophe Cestonaro <47605907+C3st0@users.noreply.github.com>
2022-04-17 13:45:52 +0100

    Update procfile

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
-app = Flask("abc")
+my_awesome_app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+
+@my_awesome_app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    my_awesome_app.run()
